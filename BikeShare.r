ec88e0622bb34cbe2f858879cc343ee05a8dea6a
Sofia Scribner <sofiadscribner@gmail.com>
2025-09-12 15:46:31 -0600

    second attempt linear regression

--- BikeShare.r
+++ BikeShare.r
@@ -92,7 +92,7 @@ type <- ggplot(df_props, aes(x = "Rentals", y = proportion, fill = Type)) +
 
 (weather + time_series) / (correlations + type)
 
-# LINEAR REGRESSION
+# LINEAR REGRESSION - FIRST ATTEMPT
 
 train <- vroom('train.csv')
 test <- vroom('test.csv')
@@ -118,4 +118,59 @@ kaggle_submission <- bike_predictions %>%
   mutate(datetime = as.character(format(datetime)))
 
 vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
+
+# LINEAR REGRESSION - SECOND ATTEMPT
+
+# cleaning
+
+train <- vroom('train.csv')
+test <- vroom('test.csv')
+
+train <- train |> select(1:9, 12)
+train <- train |> mutate(count = log(count))
+
+
+# feature engineering
+
+my_recipe <- recipe(count~., data = train) %>%
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>%
+  step_mutate(weather = factor(weather)) %>%
+  step_time(datetime, features = "hour") %>%
+  step_mutate(season = factor(season)) %>%
+  step_corr(all_numeric_predictors(), threshold=0.8)
+
+prepped_recipe <- prep(my_recipe)
+baked <- bake(prepped_recipe, new_data = test)
+
+# print baked dataset
+
+print(head(baked, n = 5))
+
+# workflow
+
+linear_model <- linear_reg() %>%
+  set_engine("lm") %>%
+  set_mode("regression")
+
+first_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(linear_model) %>%
+  fit(data = train)
+
+# make prediction
+
+lin_preds <- predict(first_workflow, new_data = test)
+lin_preds <- lin_preds %>% mutate(exp(.pred))
+
+
+# prepare for kaggle submission
+
+kaggle_submission_2 <- bike_predictions %>%
+  bind_cols(., test) %>%
+  select(datetime, .pred) %>%
+  rename(count = .pred) %>%
+  mutate(count = pmax(0, count)) %>%
+  mutate(datetime = as.character(format(datetime)))
+
+vroom_write(x=kaggle_submission_2, file="./LinearPreds.csv", delim=",")
 
